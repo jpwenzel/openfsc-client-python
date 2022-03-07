f68f4eaaaf2961ed8f3b19b9c6307cda32d690fc
Jean Pierre Wenzel <jp@pace.car>
2022-03-07 18:59:08 +0100

    Implementing HEARTBEAT response

--- openfsc-client/openfsc-client.py
+++ openfsc-client/openfsc-client.py
@@ -1,102 +1,122 @@
 import asyncio
 import logging
+from datetime import datetime, timezone
 
 import websockets
 
-logger = logging.getLogger('websockets')
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
 
-fsc_url = 'wss://fsc.sandbox.k8s.pacelink.net/ws/text'
+class OpenFscClient:
+    logger = logging.getLogger('websockets')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(logging.StreamHandler())
 
-client_supported_fsc_capabilities = {
-    'CLEAR',
-    'HEARTBEAT',
-    'LOCKPUMP',
-    'PRICES',
-    'PUMPS',
-    'PUMPSTATUS',
-    'QUIT',
-    'TRANSACTIONS',
-    'UNLOCKPUMP',
-}
+    fsc_url = 'wss://fsc.sandbox.k8s.pacelink.net/ws/text'
 
-tagged_message_counter = 0
+    client_supported_fsc_capabilities = {
+        'CLEAR',
+        'HEARTBEAT',
+        'LOCKPUMP',
+        'PRICES',
+        'PUMPS',
+        'PUMPSTATUS',
+        'QUIT',
+        'TRANSACTIONS',
+        'UNLOCKPUMP',
+    }
+
+    tagged_message_counter = 0
+
+    ws_connection = None
+
+    async def send_message(self, m):
+        await self.ws_connection.send(m)
+
+    def create_ws_message(self, m):
+        return '' + m + '\r\n'
+
+    def produce_notification_message(self, m):
+        return '* ' + m
+
+    def produce_tagged_message(self, m):
+        counter = self.tagged_message_counter
+        msg = 'C' + str(counter) + ' ' + m
+        c = str(counter)
+        print(f'> [C-REQ #{c:5}] {msg}')
+        counter += 1
+        return msg
+
+    def produce_capability_message(self):
+        m = self.produce_notification_message(
+            'CAPABILITY ' + ' '.join(str(s) for s in self.client_supported_fsc_capabilities))
+        print(f'> [C-NOTIF     ] {m}')
+        return m
+
+    def produce_charset_message(self):
+        m = self.produce_tagged_message('CHARSET UTF-8')
+        return m
+
+    async def produce_and_send_heartbeat_response_message(self, request_id):
+        timestamp = datetime.now(
+            tz=timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+        message = f'S{request_id} BEAT {timestamp}'
+        print(f'> [C-RES #{request_id:5}] {message}')
+        await self.send_message(self.create_ws_message(message))
+        return
+
+    async def parse_message(self, m):
+        chunks = m.rstrip().split(maxsplit=1)
+        seq = chunks.pop(0)
+        if str(seq).startswith('*'):
+            # Notification
+            notification_message = str(chunks.pop(0))
+            print(f'< [S-NOTIF     ] {notification_message}')
+            return
+        elif str(seq).startswith('C'):
+            # Response to client request
+            response_id = seq[1:]
+            response_message = str(chunks.pop(0))
+            print(f'< [C-RES #{response_id:5}] {response_message}')
+            return
+        elif str(seq).startswith('S'):
+            # Server Request
+            request_id = seq[1:]
+            request_message = str(chunks.pop(0))
+            print(f'< [S-REQ #{request_id:5}] {request_message}')
+            if request_message.startswith('HEARTBEAT'):
+                await self.produce_and_send_heartbeat_response_message(request_id)
+                return
+        else:
+            print(f'ERROR: message cannot be parsed: {m}')
+            return
+
+    async def consumer(self, m):
+        await self.parse_message(m)
+        return
+
+    async def producer_handler(self, websocket):
+        message = self.produce_capability_message()
+        await websocket.send(self.create_ws_message(message))
+
+        message = self.produce_charset_message()
+        await websocket.send(self.create_ws_message(message))
+
+    async def consumer_handler(self, websocket):
+        async for message in websocket:
+            decoded_message = message.decode('utf-8')
+            await self.consumer(decoded_message)
+
+    async def handler(self, websocket):
+        await asyncio.gather(
+            self.producer_handler(websocket),
+            self.consumer_handler(websocket),
+        )
+
+    async def main(self):
+        async with websockets.connect(self.fsc_url) as websocket:
+            self.ws_connection = websocket
+            await self.handler(websocket)
+            await asyncio.Future()  # run forever
 
 
-def create_ws_message(m):
-    return '' + m + '\r\n'
-
-
-def produce_notification_message(m):
-    return '* ' + m
-
-
-def produce_tagged_message(m):
-    global tagged_message_counter
-    msg = 'C' + str(tagged_message_counter) + ' ' + m
-    tagged_message_counter += 1
-    return msg
-
-
-def produce_capability_message():
-    return produce_notification_message('CAPABILITY ' + ' '.join(str(s) for s in client_supported_fsc_capabilities))
-
-
-def produce_charset_message():
-    return produce_tagged_message('CHARSET UTF-8')
-
-
-def parse_message(m):
-    chunks = m.rstrip().split(maxsplit=1)
-    seq = chunks.pop(0)
-    if str(seq).startswith('*'):
-        # Notification
-        print(f'< [notification] {chunks}')
-    elif str(seq).startswith('C'):
-        # Response
-        response_id = seq[1:]
-        print(f'< [response #{response_id}] {chunks}')
-    elif str(seq).startswith('S'):
-        # Server Request
-        request_id = seq[1:]
-        print(f'< [server request #{request_id}] {chunks}')
-    else:
-        print(f'ERROR: message cannot be parsed: {m}')
-
-
-def consumer(m):
-    parse_message(m)
-    return
-
-
-async def producer_handler(websocket):
-    message = produce_capability_message()
-    print('> ' + message)
-    await websocket.send(create_ws_message(message))
-
-    message = produce_charset_message()
-    print('> ' + message)
-    await websocket.send(create_ws_message(message))
-
-
-async def consumer_handler(websocket):
-    async for message in websocket:
-        decoded_message = message.decode('utf-8')
-        consumer(decoded_message)
-
-
-async def handler(websocket):
-    await asyncio.gather(
-        producer_handler(websocket),
-        consumer_handler(websocket),
-    )
-
-
-async def main():
-    async with websockets.connect(fsc_url) as websocket:
-        await handler(websocket)
-        await asyncio.Future()  # run forever
-
-if __name__ == '__main__':
-    asyncio.run(main())
+client = OpenFscClient()
+asyncio.run(client.main())
